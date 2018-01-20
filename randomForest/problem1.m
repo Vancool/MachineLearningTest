a455f54482d2e8087ff337406eb5abfc676aea37
vanco <173787410@qq.com>
2018-01-20 14:11:26 +0800

    random forest

--- randomForest/problem1.m
+++ randomForest/problem1.m
@@ -1,10 +1,10 @@
 clc,clear
 load('trainSet.mat');
 load('testSet.mat');
-m=length(trainSet);
+m=length(trainSet);%
 accuracySet=zeros(10,1);
-for k=1:10
-	for iteration=1:20
+for k=1:10%number of tree in random forest
+	for iteration=1:20%time of random 
 		accuracy=0;
 		for i=1:m
 			currentTrainSet=trainSet{i};
@@ -12,11 +12,14 @@ for k=1:10
 			forest=getForest(k,currentTrainSet,[1 2 3]);
 			accuracy=accuracy+getAccuracy(currentTestSet,forest,[1 2 3]);
 		end
-		accuracy=accuracy/m;
+		accuracy=accuracy/m;%get accuracy of this time random forest model
 		accuracySet(k)=accuracySet(k)+accuracy;
 	end
-	accuracySet(k)=accuracySet(k)/20;
+	accuracySet(k)=accuracySet(k)/20;%train k-random forest for 20 time and count  accuracy
 end
+%plot
+%k-the number of tree
+%y-the accuracy of random forest
 k=[1:1:10];
 plot(k,accuracySet,'r-*');
 title('k-accuracy gragh')
